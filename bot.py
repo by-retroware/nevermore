689e343456644017e1ad7c6fe658a3ae9a355089
retroware <azizahmedov2609@gmail.com>
2026-03-19 13:29:51 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -675,7 +675,7 @@ def health():
     return "OK", 200
 
 def run_flask():
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 10000))
     flask_app.run(host='0.0.0.0', port=port)
 
 # Запускаем Flask в отдельном потоке
